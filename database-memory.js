741743422192f55ada2ca8551519750400295fdd
Rafael Siqueira <rafaelsiqueira98.bm@gmail.com>
2023-09-09 20:21:05 -0300

    API Node com Postgres

---
+++ database-memory.js
@@ -0,0 +1,40 @@
+import { randomUUID } from "node:crypto"
+
+export class DatabaseMemory {
+    #videos = new Map()
+
+    list(search) {
+        return Array.from(this.#videos.entries())
+        .map((videoArray) => {
+            const id = videoArray [0]
+            const data = videoArray [1]
+
+            return {
+                id,
+                ...data,
+            }
+        })
+        .filter( video => {
+            if (search) {
+                return video.includes(search)
+            }
+
+            return true
+        })
+    }
+    create(video) {
+        const videoId = randomUUID()
+
+        this.#videos.set(videoId, video)
+    }
+
+    update(id, videos) {
+        this.#videos.set(id, video)
+    }
+
+    delete(id,) {
+        this.#videos.set(id)
+    }
+
+    
+}
